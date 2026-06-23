a30c7bbd7dad9dd1b05c9b237df543421e23654e
trenadhunter-beep <trenadhunter@gmail.com>
2026-06-23 15:44:19 +0330

    build: update Kotlin compilation jvmTarget configuration
    Migrate to the new `compilerOptions` DSL for defining the JVM target in Kotlin tasks.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,9 +62,6 @@ android {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
   }
-  kotlinOptions {
-    jvmTarget = "11"
-  }
   buildFeatures {
     compose = true
     buildConfig = true
@@ -140,4 +137,10 @@ dependencies {
   debugImplementation(libs.androidx.compose.ui.test.manifest)
   debugImplementation(libs.androidx.compose.ui.tooling)
   "ksp"(libs.androidx.room.compiler)
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+    }
 }
